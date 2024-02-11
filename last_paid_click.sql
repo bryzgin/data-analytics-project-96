0840cee715c349ad5a2e53ca1df76474371f4b10
Константин Брызгин <132559235+bryzgin@users.noreply.github.com>
2024-02-11 17:02:41 +0500

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,42 +1,41 @@
 /* формирование витрины last_paid_click.csv */
-with q1 as (
+with query as (
     select
-        s.visitor_id as visitor_id,
+        s.visitor_id,
+        s.visit_date,
         s.source as utm_source,
         s.medium as utm_medium,
         s.campaign as utm_campaign,
         s.content as utm_content,
-        max(s.visit_date) as visit_date
+        row_number()
+            over (
+                partition by s.visitor_id
+                order by s.visit_date desc
+            )
+        as visit_rank
     from sessions as s
     where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-    group by
-        visitor_id,
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        utm_content
 )
 
 select
-    q1.visitor_id,
-    q1.utm_source,
-    q1.utm_medium,
-    q1.utm_campaign,
+    q.visitor_id,
+    q.utm_source,
+    q.utm_medium,
+    q.utm_campaign,
     l.lead_id,
     l.amount,
     l.closing_reason,
-    l.status_id,
-    to_char(l.created_at, 'YYYY-MM-DD HH24:MI:SS.MS') as created_at,
-    to_char(q1.visit_date, 'YYYY-MM-DD HH24:MI:SS.MS') as visit_date
-from q1
+    l.status_id
+from query as q
 left join leads as l
     on
-        q1.visitor_id = l.visitor_id
-        and q1.visit_date >= l.created_at
+        q.visitor_id = l.visitor_id
+        and q.visit_date >= l.created_at
+where q.visit_rank = 1
 order by
     l.amount desc nulls last,
-    q1.visit_date asc,
-    q1.utm_source asc,
-    q1.utm_medium asc,
-    q1.utm_campaign asc
+    q.visit_date asc,
+    q.utm_source asc,
+    q.utm_medium asc,
+    q.utm_campaign asc
 limit 10;
